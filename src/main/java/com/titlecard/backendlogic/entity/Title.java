79b470e8411ef4a52fe6c503e2dd75a52c15c180
sahidsince2002 <120396158+sahidsince2002@users.noreply.github.com>
2026-05-27 00:06:11 +0530

    Change id type from int to Long in Title class

--- src/main/java/com/titlecard/backendlogic/entity/Title.java
+++ src/main/java/com/titlecard/backendlogic/entity/Title.java
@@ -14,7 +14,7 @@ public class Title {
     
     @Id
     @GeneratedValue(strategy=GenerationType.IDENTITY)
-    private int id;
+    private Long id;
     private String name;
 
     public String getName() {
